c1a9eaa2bf9cef7955783443aa7aff2c8468759d
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2020-11-13 15:39:06 -0600

    Se creó la conexion a la nube con el agente numero 2

--- Agente 2/Program.cs
+++ Agente 2/Program.cs
@@ -1,26 +1,32 @@
 ﻿using System;
+using Google.Cloud.Firestore;
 using Experimental.System.Messaging;
 
 namespace Agente2
 {
     class Agente2
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             Console.Clear();
-            string archivo = ""; 
             MessageQueue cola = new MessageQueue(".\\private$\\Nombres.Aleatorios");
             Funcionalidades Funciones = new Funcionalidades();
-            archivo = Funciones.Recibir(cola, archivo);
-            Console.WriteLine(archivo);
+            FirestoreDb db = FirestoreDb.Create("examenfinal-angelperez");
+            await Funciones.EnviarAlaNubeAsync(cola, db);
         }
     }
     public class Funcionalidades{
-        public String Recibir(MessageQueue Cola, string x){
+        public String Recibir(MessageQueue Cola){
             Message mensaje = Cola.Receive();
             mensaje.Formatter = new XmlMessageFormatter(new String[] {"System.String,mscorlib"} );
-            x = mensaje.Body.ToString();
+            string x = mensaje.Body.ToString();
             return x;
+        }
+        public async System.Threading.Tasks.Task EnviarAlaNubeAsync(MessageQueue cola, FirestoreDb db){
+            CollectionReference collection = db.Collection("Archivos");
+            String archivo = Recibir(cola);
+            DocumentReference document = await collection.AddAsync(new {Archivo = archivo});
+            DocumentSnapshot snapshot = await document.GetSnapshotAsync();
         }
     }
 }
