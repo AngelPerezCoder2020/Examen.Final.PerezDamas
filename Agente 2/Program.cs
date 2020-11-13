45da75861d84931b7f30030e439cb4f2fc59e71b
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2020-11-13 13:50:16 -0600

    Se crea el segundo agente

--- Agente 2/Program.cs
+++ Agente 2/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using Experimental.System.Messaging;
 
-namespace Agente_2
+namespace Agente2
 {
-    class Program
+    class Agente2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            string archivo = ""; 
+            MessageQueue cola = new MessageQueue(".\\private$\\Nombres.Aleatorios");
+            Funcionalidades Funciones = new Funcionalidades();
+            archivo = Funciones.Recibir(cola, archivo);
+            Console.WriteLine(archivo);
+        }
+    }
+    public class Funcionalidades{
+        public String Recibir(MessageQueue Cola, string x){
+            Message mensaje = Cola.Receive();
+            mensaje.Formatter = new XmlMessageFormatter(new String[] {"System.String,mscorlib"} );
+            x = mensaje.Body.ToString();
+            return x;
         }
     }
 }
